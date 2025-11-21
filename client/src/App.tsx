f82629869f311adf823e764c48dbc9669bbb9ce0
Mamata1980 <sahumilan941@gmail.com>
2025-11-21 11:09:16 +0530

    Create App component with Chat integration

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Chat from './components/Chat';
+
+export default function App() {
+  return (
+    <div style={{ height: '100vh' }}>
+      <Chat />
+    </div>
+  );
+}
